bdbc7ebab1e82e3d7bfb7c400b7f76574c94d205
Gabriel Genê <gabrielgene97@gmail.com>
2018-04-14 14:56:56 -0300

    Use port env

--- src/config/index.js
+++ src/config/index.js
@@ -4,7 +4,7 @@ const env = (key, miss) => process.env[key] || miss;
 const reducer = (obj, miss, key) => ({ ...obj, [key]: env(key, miss) });
 
 const config = reduce({
-  PORT: 4000,
+  PORT: process.env.PORT || 4000,
   SECRET_KEY: 'local-secret-key',
   BCRYPT_SALT_ROUNDS: 5,
   MONGO_URL: 'mongodb://admin:bill!00@ds237989.mlab.com:37989/bill-dev',
